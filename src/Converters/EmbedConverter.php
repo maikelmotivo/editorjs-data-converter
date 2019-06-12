2695819ce04e88a6878a2d8b658db5354488f6ed
Maikel Oelen <maikel@motivo.nl>
2019-06-12 14:36:47 +0200

    resolved discussions

--- src/Converters/EmbedConverter.php
+++ src/Converters/EmbedConverter.php
@@ -1,11 +1,11 @@
 <?php
 
-
 namespace Motivo\EditorJsDataConverter\Converters;
-
 
 use Motivo\EditorJsDataConverter\Converters\Contracts\Converter;
 use Motivo\EditorJsDataConverter\Traits\WithHtml;
+use Spatie\Html\Elements\Element;
+use Illuminate\Support\Arr;
 
 class EmbedConverter implements Converter
 {
@@ -14,23 +14,25 @@ class EmbedConverter implements Converter
     public function toHtml(array $itemData): string
     {
         $container = $this->getContainer();
-        $iframe = $this->getIframe($itemData['service'], $itemData['embed']);
-        $caption = $this->getCaption($itemData['caption']);
+
+        $iframe = $this->getIframe(Arr::get($itemData, 'service'), Arr::get($itemData, 'embed'));
+
+        $caption = $this->getCaption(Arr::get($itemData, 'caption'));
 
         return $container->child([$iframe, $caption]);
     }
 
-    private function getContainer()
+    private function getContainer(): Element
     {
         return $this->html->element('div')->class('embed-container');
     }
 
-    private function getIframeContainer()
+    private function getIframeContainer(): Element
     {
         return $this->html->element('div')->class('embed-responsive embed-responsive-16by9');
     }
 
-    private function getCaption(?string $caption)
+    private function getCaption(?string $caption): ?Element
     {
         if ($caption) {
             return $this->html->element('p')->html($caption)->class('caption');
@@ -39,7 +41,7 @@ class EmbedConverter implements Converter
         return null;
     }
 
-    private function getIframe(string $service, string $url)
+    private function getIframe(string $service, string $url): Element
     {
         $container = $this->getIframeContainer();
         $iframe = $this->{'get' . ucfirst($service)}($url);
@@ -47,7 +49,7 @@ class EmbedConverter implements Converter
         return $container->child($iframe);
     }
 
-    private function getYoutube(string $url)
+    private function getYoutube(string $url): Element
     {
         $attrs = [
             'src' => $url,
